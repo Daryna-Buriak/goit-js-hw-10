4b5ff2dac36cea5f676ba4f23998ee3bf6548e3f
Daryna <buriak.daryna@gmail.com>
2025-01-14 19:37:58 +0200

    Add task 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,3 +2,42 @@
 import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const delayInput = document.querySelector('.delay-input');
+const createBtn = document.querySelector('.button-styles');
+
+//console.dir(delayInput);
+//console.dir(createBtn);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const selectedDelay = Number(delayInput.value);
+  const state = document.querySelector('input[name="state"]:checked');
+  const selectedState = state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (selectedState === 'fulfilled') {
+        promise.then(
+          iziToast.success({
+            title: 'OK',
+            message: `Fulfilled promise in ${selectedDelay}ms`,
+          })
+        );
+      } else {
+        promise.catch(
+          iziToast.error({
+            title: 'Error',
+            message: `Rejected promise in ${selectedDelay}ms`,
+          })
+        );
+      }
+    }, selectedDelay);
+    form.reset();
+  });
+
+  //console.log(selectedDelay);
+  //console.log(state);
+  //console.log(selectedState);
+});
